c839a479746f3716b0d9a0cb96fcd74b2771d528
suneel0101 <suneel0101@gmail.com>
2012-07-07 20:08:13 -0400

    modified gitignore, added local settings and skeleton of deployment command

---
+++ settings/local_settings.py
@@ -0,0 +1,12 @@
+from settings.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'default_db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
