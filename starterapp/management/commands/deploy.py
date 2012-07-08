a9331a8280fddfcbd7118d8536b1767079bb5deb
suneel0101 <suneel0101@gmail.com>
2012-07-07 20:46:41 -0400

    removing ipdb...

--- starterapp/management/commands/deploy.py
+++ starterapp/management/commands/deploy.py
@@ -5,8 +5,7 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        #os.system('heroku create')
-        import ipdb; ipdb.set_trace()
+        os.system('heroku create')
         os.system('git add .')
         os.system("git commit -m 'first commit to heroku'")
         os.system('git push heroku master')
